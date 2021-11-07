7c5d1aa51ab2e20e68779ec26790f4fe16fe71d3
KaldoraNg <90674300+KaldoraNg@users.noreply.github.com>
2021-11-08 00:48:51 +0800

    -

--- services/unit_test.py
+++ services/unit_test.py
@@ -59,56 +59,5 @@ class QuestionsTestCase(unittest.TestCase):
         db.drop_all()
 # Lim Zhi Hao (END)
 
-class ClassTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-        db.create_all()
-        self.assigning_learner = {
-                                "learner_id" : 1,
-                                "engineer_id" : 1,
-                                "learner_name" : "LingLi",
-                                "course_id" : 1,
-                                "class_id" : 1,
-                                "assigned" : 1,
-                                "approved" : 1,
-                                "course_completed"
-                            }
-        self.retrieve_all = {
-                                "course_id" : 1
-                            }
-        self.retrieve_learner = {
-                                    "learner_id" : 1
-                                }
-
-    # Create a True/False question in a quiz
-    def test_question_creation_tf(self):
-        res = self.app.post('/quiz/createQuestion', data=json.dumps(dict(self.question_tf)), content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('A', str(res.data))
-
-    # Create an MCQ question in a quiz
-    def test_question_creation_mcq(self):
-        res = self.app.post('/quiz/createQuestion', data=json.dumps(dict(self.question_mcq)), content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('Eat', str(res.data))
-
-    # Retrieve all questions for specific quiz on create_quiz.html
-    def test_retrieve_all_questions(self):
-        self.app.post('/quiz/createQuestion', data=json.dumps(dict(self.question_tf)), content_type='application/json')
-        res = self.app.post('/quiz/retrieveAllQuestions', data=json.dumps(dict(self.retrieve_all)), content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('A', str(res.data))
-
-    # Retrieve specific question for specific quiz on create_quiz.html
-    def test_retrieve_question(self):
-        self.app.post('/quiz/createQuestion', data=json.dumps(dict(self.question_mcq)), content_type='application/json')
-        res = self.app.post('/quiz/retrieveQuestion', data=json.dumps(dict(self.retrieve_question)), content_type='application/json')
-        self.assertEqual(res.status_code, 201)
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
 if __name__ == "__main__":
     unittest.main()
